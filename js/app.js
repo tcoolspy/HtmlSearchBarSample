0afcde72a50b7ee57a154a5033bdda37bde00c3b
Travis Spencer <tcoolspy@gmail.com>
2024-04-24 09:08:54 -0500

    adding some basic styling..tailwind

--- js/app.js
+++ js/app.js
@@ -24,6 +24,7 @@ function search(e) {
 function showSuggestions(results, inputVal) {
   for (let result of results) {
     var li = document.createElement("li");
+    li.style.border = "1px solid black";
     li.addEventListener("click", useSuggestions);  // NOTE: I'm adding an event listener on each list item.
     li.appendChild(document.createTextNode(result));
     suggestions.appendChild(li);
---
+++ readme.txt
@@ -0,0 +1,3 @@
+use this command to start the tailwind compiler
+
+npx tailwindcss -i ./css/style.css -o ./css/output.css --watch
